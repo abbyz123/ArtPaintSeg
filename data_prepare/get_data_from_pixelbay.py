6393c46fb4d5b82421c3532f1c974c5f96b1b6b9
mengyuan zhang <mengyuanzhang@mengyuans-imac.lan>
2021-07-13 22:25:17 -0700

    add train, test, valid structure for downloaded image dataset

--- data_prepare/get_data_from_pixelbay.py
+++ data_prepare/get_data_from_pixelbay.py
@@ -16,8 +16,17 @@ with open(path_to_category) as f:
 
 # create download folder
 path_to_download = curr_path.joinpath(r'../Download')
+path_to_train = path_to_download.joinpath('train')
+path_to_test = path_to_download.joinpath('test')
+path_to_valid = path_to_download.joinpath('valid')
 if not os.path.isdir(path_to_download):
     os.mkdir(path_to_download)
+if not os.path.isdir(path_to_train):
+    os.mkdir(path_to_train)
+if not os.path.isdir(path_to_test):
+    os.mkdir(path_to_test)
+if not os.path.isdir(path_to_valid):
+    os.mkdir(path_to_valid)
 
 # download photo for each category
 for item in categories:
@@ -29,7 +38,7 @@ for item in categories:
         jpg_req = requests.get(jpg_url, timeout=60)
         
         # create folder for catetory
-        path_to_category = path_to_download.joinpath(item)
+        path_to_category = path_to_train.joinpath(item)
         if not os.path.isdir(path_to_category):
             os.mkdir(path_to_category)
 
